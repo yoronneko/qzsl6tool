609ce235d4d5ddc099b06e04d7818776649f8fe5
Satoshi Takahashi <git@s-taka.org>
2022-12-12 20:37:49 +0900

    CODEDIR in test/do_test.sh

--- test/do_test.sh
+++ test/do_test.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+CODEDIR=../python/
+#CODEDIR=
+
 do_test() {
     CODE=$1
     EXT_FROM=$2
@@ -10,14 +13,14 @@ do_test() {
     echo -n "  ${BASENAME}.${EXT_FROM}: "
     cat ${SRCDIR}${BASENAME}.${EXT_FROM} | ${CODE} ${ARG} \
         > ${BASENAME}.${EXT_TO}
-    diff -q ${BASENAME}.${EXT_TO} expect/${BASENAME}.${EXT_TO} \
+    diff -q ${BASENAME}.${EXT_TO} expect/${BASENAME}.${EXT_TO} > /dev/null \
         && echo "Passed." \
         || echo "Failed."
     rm ${BASENAME}.${EXT_TO}
 }
 
 # ------ test 1: pksdr2qzsl6.py ------
-CODE=../python/pksdr2qzsl6.py ARG= EXT_FROM=txt EXT_TO=l6
+CODE=${CODEDIR}pksdr2qzsl6.py ARG= EXT_FROM=txt EXT_TO=l6
 echo "1. Pocket SDR to QZS L6 message conversion (${CODE})"
 
 SRCDIR=../sample/
@@ -29,7 +32,7 @@ do_test $CODE $EXT_FROM $EXT_TO $BASENAME $SRCDIR $ARG
 
 echo ""
 # ------ test 2: alst2qzsl6.py ------
-CODE=../python/alst2qzsl6.py ARG=-l EXT_FROM=alst EXT_TO=l6
+CODE=${CODEDIR}alst2qzsl6.py ARG=-l EXT_FROM=alst EXT_TO=l6
 echo "2. Allystar to QZS L6 message conversion (${CODE})"
 
 SRCDIR=../sample/
@@ -43,9 +46,9 @@ BASENAME=20221130-125237mdc-ppp
 do_test $CODE $EXT_FROM $EXT_TO $BASENAME $SRCDIR $ARG
 
 echo ""
-# ------ test 3: qzsl62rtcm.py L6 message ------
-CODE=../python/qzsl62rtcm.py ARG='-t 2' EXT_FROM=l6 EXT_TO=txt
-echo "3. QZS L6 message dump (${CODE})"
+# ------ test 3: qzsl62rtcm.py L6 message dump with -t 2 option ------
+CODE=${CODEDIR}qzsl62rtcm.py ARG='-t 2' EXT_FROM=l6 EXT_TO=txt
+echo "3. QZS L6 message dump with -t 2 option (${CODE})"
 
 SRCDIR=expect/
 BASENAME=20220326-231200clas
@@ -59,8 +62,8 @@ do_test $CODE $EXT_FROM $EXT_TO $BASENAME $SRCDIR $ARG
 
 echo ""
 # ------ test 4: qzsl62rtcm.py RTCM conversion ------
-CODE=../python/qzsl62rtcm.py ARG='-r' EXT_FROM=l6 EXT_TO=rtcm
-echo "4. QZS L6 to RTCM3 message conversion (${CODE})"
+CODE=${CODEDIR}qzsl62rtcm.py ARG='-r' EXT_FROM=l6 EXT_TO=rtcm
+echo "4. QZS L6 to RTCM message conversion (${CODE})"
 
 SRCDIR=expect/
 BASENAME=20220326-231200clas
@@ -74,7 +77,7 @@ do_test $CODE $EXT_FROM $EXT_TO $BASENAME $SRCDIR $ARG
 
 echo ""
 # ------ test 5: showrtcm.py RTCM message ------
-CODE=../python/showrtcm.py ARG= EXT_FROM=rtcm EXT_TO=rtcm.txt
+CODE=${CODEDIR}showrtcm.py ARG= EXT_FROM=rtcm EXT_TO=rtcm.txt
 echo "5. RTCM message dump (${CODE})"
 
 SRCDIR=expect/
