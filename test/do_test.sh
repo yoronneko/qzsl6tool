e29e94c7bfc2950d784a8758ae6ce3880cbb82e0
Satoshi Takahashi <git@s-taka.org>
2023-09-07 21:42:49 +0900

    modified test script and data

--- test/do_test.sh
+++ test/do_test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 CODEDIR=../python/
-#CODEDIR=
+CODEDIR=
 
 ESC=$(printf '\033')
 
@@ -26,8 +26,8 @@ do_test() {
 
 # ------
 CODE=${CODEDIR}psdrread.py ARG=-l EXT_FROM=psdr EXT_TO=l6
-echo "Pocket SDR to QZS L6 message conversion (${CODE} ${ARG})"
-
+echo "Pocket SDR log data conversion:"
+echo "- QZS L6 (${CODE} ${ARG})"
 SRCDIR=../sample/
 BASENAME=20211226-082212clas
 do_test $CODE $EXT_FROM $EXT_TO $BASENAME $SRCDIR $ARG
@@ -35,11 +35,16 @@ do_test $CODE $EXT_FROM $EXT_TO $BASENAME $SRCDIR $ARG
 BASENAME=20211226-082212mdc
 do_test $CODE $EXT_FROM $EXT_TO $BASENAME $SRCDIR $ARG
 
-# ------
-CODE=${CODEDIR}alstread.py ARG=-l EXT_FROM=alst EXT_TO=l6
-echo ""
-echo "Allystar to QZS L6 message conversion (${CODE} ${ARG})"
+ARG='-e' EXT_TO=e6b
+echo "- GAL E6B (${CODE} ${ARG})"
+BASENAME=20230305-063900has
+do_test $CODE $EXT_FROM $EXT_TO $BASENAME $SRCDIR $ARG
 
+# ------
+echo ""
+CODE=${CODEDIR}alstread.py ARG=-l EXT_FROM=alst EXT_TO=l6
+echo "Allystar raw data conversion:"
+echo "- QZS L6 (${CODE} ${ARG})"
 SRCDIR=../sample/
 BASENAME=20220326-231200clas
 do_test $CODE $EXT_FROM $EXT_TO $BASENAME $SRCDIR $ARG
@@ -51,36 +56,38 @@ BASENAME=20221130-125237mdc-ppp
 do_test $CODE $EXT_FROM $EXT_TO $BASENAME $SRCDIR $ARG
 
 # ------
-CODE=${CODEDIR}novread.py ARG='' EXT_FROM=nov EXT_TO=txt
 echo ""
-echo "NovAtel raw message read (${CODE})"
-
+CODE=${CODEDIR}novread.py ARG=-e EXT_FROM=nov EXT_TO=e6b
+echo "NovAtel raw data conversion:"
+echo "- GAL E6B (${CODE} ${ARG})"
 SRCDIR=../sample/
 BASENAME=20230819-053733has
 do_test $CODE $EXT_FROM $EXT_TO $BASENAME $SRCDIR $ARG
 
-BASENAME=20230819-061342misc
+BASENAME=20230819-053733has
 do_test $CODE $EXT_FROM $EXT_TO $BASENAME $SRCDIR $ARG
 
 # ------
-CODE=${CODEDIR}septread.py ARG='' EXT_FROM=sept EXT_TO=txt
 echo ""
-echo "Septentrio raw data read (${CODE})"
-
+CODE=${CODEDIR}septread.py ARG=-l EXT_FROM=sept EXT_TO=l6
+echo "Septentrio raw data conversion:"
+echo "- QZS L6 (${CODE} ${ARG})"
 SRCDIR=../sample/
-BASENAME=20230819-081730hasbds
-do_test $CODE $EXT_FROM $EXT_TO $BASENAME $SRCDIR $ARG
-
 BASENAME=20230819-082130clas
 do_test $CODE $EXT_FROM $EXT_TO $BASENAME $SRCDIR $ARG
 
 BASENAME=20230819-085030mdc-ppp
 do_test $CODE $EXT_FROM $EXT_TO $BASENAME $SRCDIR $ARG
 
+ARG=-e EXT_TO=e6b
+echo "- GAL E6B (${CODE} ${ARG})"
+BASENAME=20230819-081730hasbds
+do_test $CODE $EXT_FROM $EXT_TO $BASENAME $SRCDIR $ARG
+
 # ------
-CODE=${CODEDIR}qzsl6read.py ARG='-t 2' EXT_FROM=l6 EXT_TO=txt
 echo ""
-echo "QZS L6 message read (${CODE} ${ARG})"
+CODE=${CODEDIR}qzsl6read.py ARG='-t 2' EXT_FROM=l6 EXT_TO=txt
+echo "QZS L6 message read (${CODE} ${ARG}):"
 
 SRCDIR=expect/
 BASENAME=20220326-231200clas
@@ -92,9 +99,15 @@ do_test $CODE $EXT_FROM $EXT_TO $BASENAME $SRCDIR $ARG
 BASENAME=20221130-125237mdc-ppp
 do_test $CODE $EXT_FROM $EXT_TO $BASENAME $SRCDIR $ARG
 
+BASENAME=20230819-082130clas
+do_test $CODE $EXT_FROM $EXT_TO $BASENAME $SRCDIR $ARG
+
+BASENAME=20230819-085030mdc-ppp
+do_test $CODE $EXT_FROM $EXT_TO $BASENAME $SRCDIR $ARG
+
 # ------
-CODE=${CODEDIR}qzsl6read.py ARG='-r' EXT_FROM=l6 EXT_TO=rtcm
 echo ""
+CODE=${CODEDIR}qzsl6read.py ARG='-r' EXT_FROM=l6 EXT_TO=rtcm
 echo "QZS L6 to RTCM message conversion (${CODE} ${ARG})"
 
 SRCDIR=expect/
@@ -110,7 +123,7 @@ do_test $CODE $EXT_FROM $EXT_TO $BASENAME $SRCDIR $ARG
 # ------
 CODE=${CODEDIR}rtcmread.py ARG= EXT_FROM=rtcm EXT_TO=rtcm.txt
 echo ""
-echo "RTCM message read (${CODE})"
+echo "RTCM message read (${CODE} ${ARG})"
 
 SRCDIR=expect/
 BASENAME=20220326-231200clas
@@ -124,6 +137,18 @@ do_test $CODE $EXT_FROM $EXT_TO $BASENAME $SRCDIR $ARG
 
 SRCDIR=../sample/
 BASENAME=20221213-010900
+do_test $CODE $EXT_FROM $EXT_TO $BASENAME $SRCDIR $ARG
+
+# ------
+CODE=${CODEDIR}gale6read.py ARG='-t 2' EXT_FROM=e6b EXT_TO=txt
+echo ""
+echo "GAL E6 message read (${CODE} ${ARG})"
+
+SRCDIR=expect/
+BASENAME=20230305-063900has
+do_test $CODE $EXT_FROM $EXT_TO $BASENAME $SRCDIR $ARG
+
+BASENAME=20230819-081730hasbds
 do_test $CODE $EXT_FROM $EXT_TO $BASENAME $SRCDIR $ARG
 
 # ------
@@ -152,10 +177,10 @@ BASENAME=20220930-115617has
 do_test $CODE $EXT_FROM $EXT_TO $BASENAME $SRCDIR $ARG
 
 BASENAME=20230305-063900has
-do_test $CODE $EXT_FROM $EXT_TO $BASENAME $SRCDIR $ARG
+#do_test $CODE $EXT_FROM $EXT_TO $BASENAME $SRCDIR $ARG
 
 # ------
-CODE=${CODEDIR}nov2has.py ARG='-t 2' EXT_FROM=nov EXT_TO=decoded.txt
+CODE=${CODEDIR}nov2has.py ARG='-t 2' EXT_FROM=nov EXT_TO=txt
 echo ""
 echo "NovAtel HAS message read (${CODE} ${ARG})"
 
